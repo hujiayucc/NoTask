dadf7349e2398f3b09200cf0e6e0b8fe4c40380c
hujiayucc <hujiayucc@qq.com>
2024-06-23 13:48:11 +0800

    Fix Bugs

--- app/src/main/java/com/hujiayucc/notask/MainHook.java
+++ app/src/main/java/com/hujiayucc/notask/MainHook.java
@@ -10,28 +10,40 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class MainHook implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
-        XposedHelpers.findAndHookMethod(Activity.class, "onKeyDown", int.class, KeyEvent.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                super.beforeHookedMethod(param);
-                int code = (int) param.args[0];
-                int action = ((KeyEvent) param.args[1]).getAction();
-                if (code == KeyEvent.KEYCODE_BACK && action == KeyEvent.ACTION_DOWN) {
-                    Activity activity = (Activity) param.thisObject;
-                    excludeFromRecent(activity, true);
-                    XposedBridge.log("NoTask:" + activity.getPackageName());
-                    param.setResult(false);
-                    activity.finish();
+        String packageName = lpparam.packageName;
+        String mainActivity = Packages.find(packageName);
+        Class<?> cls = (mainActivity != null) ? XposedHelpers.findClass(mainActivity, lpparam.classLoader) : Activity.class;
+        try {
+        	XposedHelpers.findAndHookMethod(cls, "onKeyDown", int.class, KeyEvent.class, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    super.afterHookedMethod(param);
+                    int code = (int) param.args[0];
+                    int action = ((KeyEvent) param.args[1]).getAction();
+                    if (code == KeyEvent.KEYCODE_BACK && action == KeyEvent.ACTION_DOWN) {
+                        Activity activity = (Activity) param.thisObject;
+                        excludeFromRecent(activity, true);
+                        XposedBridge.log("NoTask: " + packageName + "." + activity.getClass().getSimpleName());
+                    }
                 }
-            }
-        });
+            });
+        } catch(NoSuchMethodError e) {}
+        
+        try {
+            XposedHelpers.findAndHookMethod(cls, "dispatchKeyEvent", KeyEvent.class, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    super.afterHookedMethod(param);
+                    KeyEvent event = (KeyEvent) param.args[0];
+                    if (event.getKeyCode() == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+                        Activity activity = (Activity) param.thisObject;
+                        excludeFromRecent(activity, true);
+                    }
+                }
+            });
+        } catch(NoSuchMethodError e) {}
 
         XposedHelpers.findAndHookMethod(Activity.class, "onStart", new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                super.beforeHookedMethod(param);
-            }
-
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 super.afterHookedMethod(param);
@@ -47,8 +59,8 @@ public class MainHook implements IXposedHookLoadPackage {
             for (ActivityManager.AppTask appTask : manager.getAppTasks()) {
                 appTask.setExcludeFromRecents(exclude);
             }
+            XposedBridge.log("NoTask: [" + ((exclude) ? "Hide" : "Display") + "] " + activity.getPackageName() + "." + activity.getClass().getSimpleName());
         } catch (Exception e) {
-            e.printStackTrace();
             XposedBridge.log(e.getMessage());
         }
     }
